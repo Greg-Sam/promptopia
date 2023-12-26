0863aebc270f72338779e6f222cea9651d7ef875
Greg Samuelson <grssam@gmail.com>
2023-12-26 14:11:36 -0800

    google auth working

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,26 +7,26 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
 
-  const isUserLoggedIn = true
+  const { data: session } = useSession()
 
   const [providers, setProviders] = useState(null)
-  const [toggleDropdown, settoggleDropdown] = useState(false)
+  const [toggleDropdown, setToggleDropdown] = useState(false)
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders()
 
       setProviders(response)
     }
 
-    setProviders()
+    setUpProviders()
   }, [])
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href='/' className='flex gap-2'>
         <Image
-          src='/assets/images/logo.svg'
-          alt="Promptopia Logo"
+          src={session?.user.image}
+          alt="User Image"
           width={30}
           height={30}
           className="object-contain"
@@ -34,9 +34,12 @@ const Nav = () => {
         <p className="logo_text">Promptopia</p>
       </Link>
 
+
+   
+
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt"
               className='black_btn'>
@@ -47,7 +50,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image
-                src='/assets/images/logo.svg'
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className='rounded-full'
@@ -77,7 +80,7 @@ const Nav = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
               src='/assets/images/logo.svg'
