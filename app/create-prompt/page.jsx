b1991da12c7ce51802ee9fd4c1f5b195b180ea56
Greg Samuelson <grssam@gmail.com>
2023-12-27 13:00:04 -0800

    form and serverless connection to mongo

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,11 +2,14 @@
 
 import { useState } from 'react'
 import { useSession } from 'next-auth/react'
-import { userRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 import Form from '@components/Form'
 
 export const CreatePrompt = () => {
+  const router = useRouter()
+  const { data: session } = useSession()
+
   const [submitting, setSubmitting] = useState(false)
 
   const [post, setPost] = useState({
@@ -15,7 +18,29 @@ export const CreatePrompt = () => {
   })
 
   const createPrompt = async (e) => {
+    e.preventDefault()
+    setSubmitting(true)
+    
+    try {
+      const response = await fetch('/api/prompt/new',
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            prompt: post.prompt,
+            userId: session?.user.id,
+            tag: post.tag
+          })
+        })
+      if (response.ok) {
+        router.push('/')
+      }
 
+
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setSubmitting(false)
+    }
   }
   return (
     <Form
